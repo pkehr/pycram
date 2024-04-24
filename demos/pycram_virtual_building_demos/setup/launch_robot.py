1c6df46c54ba40657697d228a5dc0b62d7348fa6
kecks <pkehr@uni-bremen.de>
2024-04-24 15:49:12 +0200

    [setup]: added ability and resources to launch rviz + robot from script

--- demos/pycram_virtual_building_demos/setup/launch_robot.py
+++ demos/pycram_virtual_building_demos/setup/launch_robot.py
@@ -1,18 +1,11 @@
 import time
+from enum import Enum, auto
+from typing import List, Tuple
+
 import roslaunch
 import rospy
-import rospkg
 
-
-def launch_pr2():
-    """
-    Method to launch PR2 on the parameter server and start the ik service
-    """
-    # name = 'pr2'
-    # urdf = 'pr2.urdf'
-    executable = 'pr2_standalone.launch'
-    launch_robot(executable)
-
+from roslaunch.parent import ROSLaunchParent
 
 # For future work / robots
 # def launch_hsrb():
@@ -28,25 +21,68 @@ def launch_pr2():
 #    executable = 'armar6_standalone.launch'
 #    launch_robot(executable)
 
+class Robots(Enum):
+    PR2 = auto()
 
-def launch_robot(launch_file, package='pycram', launch_folder='/launch/'):
-    """
-    General method to start a specified launch file with given parameters.
-    Default location for launch files here is in the folder 'launch' inside the pycram package
 
-    :param launch_file: File name of the launch file
-    :param package: Name of the package
-    :param launch_folder: Location of the launch file inside the package
-    """
+class RvizConfig(Enum):
+    LOCAL = auto()
+    WEB = auto()
+    WITHOUT = auto()
 
-    rospath = rospkg.RosPack()
 
-    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    roslaunch.configure_logging(uuid)
-    launch = roslaunch.parent.ROSLaunchParent(uuid, [rospath.get_path(package) + launch_folder + launch_file])
-    launch.start()
+class LaunchRobot:
+    def __init__(self, robot: Robots, rviz: RvizConfig):
+        self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(self.uuid)
+        launch_files = self.__get_launch_files(robot, rviz)
 
-    rospy.loginfo(f'{launch_file} started')
+        self.parent: ROSLaunchParent = roslaunch.parent.ROSLaunchParent(self.uuid, launch_files)
 
-    # Wait for ik server to launch
-    time.sleep(2)
+    def start(self):
+        self.parent.start()
+        rospy.sleep(5)
+
+    def shutdown(self):
+        self.parent.shutdown()
+
+    def __get_launch_files(self, robot, rviz) -> List:
+        launch_files = []
+        self.pycram_pkg = 'pycram'
+
+        robot_config = self.__get_robot_config(robot)
+        rviz_config = self.__get_rviz_config(rviz)
+
+        if robot_config is not None:
+            launch_files.append(robot_config)
+
+        if rviz_config is not None:
+            launch_files.append(rviz_config)
+
+        return launch_files
+
+    def __get_robot_config(self, robot):
+        robot_launch_file = None
+
+        if robot == Robots.PR2:
+            cli_robot = [self.pycram_pkg, 'pr2_standalone.launch']
+            robot_launch_file = roslaunch.rlutil.resolve_launch_arguments(cli_robot)[0]
+
+        return robot_launch_file
+
+    def __get_rviz_config(self, rviz):
+        rviz_launch_file = None
+
+        if rviz == RvizConfig.LOCAL:
+            cli_rviz = [self.pycram_pkg, "local_rviz.launch"]
+            rviz_launch_file = roslaunch.rlutil.resolve_launch_arguments(cli_rviz)[0]
+
+        elif rviz == RvizConfig.WEB:
+            cli_rviz = ['rvizweb', 'rvizweb.launch', 'config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json']
+
+            rviz_launch_file = (roslaunch.rlutil.resolve_launch_arguments(cli_rviz)[0], cli_rviz[2:])
+
+        elif rviz == RvizConfig.WITHOUT:
+            rospy.logdebug('launching without rviz')
+
+        return rviz_launch_file
