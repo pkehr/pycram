559b046dd9e094c69266cc7eaf4d3e264a4047c8
kecks <pkehr@uni-bremen.de>
2024-05-30 11:43:03 +0200

    [mulitrobot_demo]: added switching actions between tiago and pr2

--- demos/pycram_multirobot_demo/multirobot_demo.py
+++ demos/pycram_multirobot_demo/multirobot_demo.py
@@ -3,21 +3,20 @@ import rospy
 from pycram.datastructures.enums import WorldMode
 from pycram.datastructures.pose import Pose
 from pycram.process_module import simulated_robot
-from setup import ROBOTS, actions, create_robot
 from pycram.worlds.bullet_world import BulletWorld
+from setup import ROBOTS, actions, create_robot
 
 world = BulletWorld(WorldMode.GUI)
 
 pose_pr2 = Pose([0, 1, 0])
 pose_tiago = Pose([0, 3, 0])
 
+print("Set first robot")
 robot_pr2 = create_robot(ROBOTS.PR2, pose=pose_pr2)
 # current_environment = set_environment(ENVIRONMENTS.KITCHEN)
-print("Set first robot")
 rospy.sleep(5)
 print("Set second robot")
 robot_tiago = create_robot(ROBOTS.TIAGO, pose=pose_tiago)
-
 rospy.sleep(3)
 print("pr2 actions")
 with simulated_robot(robot_pr2):
@@ -26,6 +25,7 @@ with simulated_robot(robot_pr2):
 rospy.sleep(3)
 print("tiago actions")
 with simulated_robot(robot_tiago):
-    actions(park=True)
+    actions(park=True, torso=True)
 
-
+with simulated_robot(robot_pr2):
+    actions(torso=True)
