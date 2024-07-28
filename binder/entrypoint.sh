4aca3216eef9bfe9a8ddd40a97a64fab16942a28
kecks <pkehr@uni-bremen.de>
2024-07-28 04:22:51 +0200

    [binder] added notebook

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,7 +3,7 @@
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
-roslaunch pycram pr2.launch & 
+roslaunch --wait pycram pr2.launch & 
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
 
 xvfb-run exec "$@"
