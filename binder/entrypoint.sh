8474d89c88a4607223ea6e1445d8657bc0d26995
kecks <pkehr@uni-bremen.de>
2024-07-28 04:16:18 +0200

    [binder_launch] removed unused launch file

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -4,7 +4,6 @@ source ${PYCRAM_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
 roslaunch pycram pr2.launch & 
-roslaunch pycram tiago.launch &
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
 
 xvfb-run exec "$@"
