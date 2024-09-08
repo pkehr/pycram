1b40113e9de68852e18aabcdd6dd1a3e703aac12
pkehr <philk99@web.de>
2024-09-08 22:30:00 +0200

    [multirobot-demo] added demo template for hsrb + turtle transporting

--- demos/pycram_multirobot_real_demo/scenarios/hsrb_turtle_transporting.py
+++ demos/pycram_multirobot_real_demo/scenarios/hsrb_turtle_transporting.py
@@ -1,83 +1,132 @@
-import rospy
-from gym.pybullet_robots.xarm.xarm import table_pos
-
-from demos.pycram_multirobot_demo.setup.actions import actions
-from demos.utils.enums import ROBOTS, ENVIRONMENTS
-from demos.utils.launcher import launch_robot
-from demos.utils.object_spawner import set_environment, create_robot
 from pycram.datastructures.dataclasses import Color
-from pycram.datastructures.enums import ObjectType, Arms, Grasp
-from pycram.datastructures.pose import Pose
-from pycram.designators.action_designator import NavigateAction, TransportAction, LookAtAction, DetectAction
-from pycram.designators.object_designator import BelieveObject
-from pycram.process_module import real_robot, with_real_robot
+from pycram.datastructures.enums import Arms
+from pycram.datastructures.enums import ObjectType, WorldMode
+from pycram.designators.action_designator import *
+from pycram.designators.object_designator import *
+from pycram.external_interfaces.navigate import PoseNavigator
+# new imports
+from pycram.object_descriptors.urdf import ObjectDescription
+from pycram.process_module import real_robot
+from pycram.ros.robot_state_updater import RobotStateUpdater
+from pycram.utilities.robocup_utils import TextToSpeechPublisher, ImageSwitchPublisher, \
+    HSRBMoveGripperReal, rotated_quaternion
 from pycram.world_concepts.world_object import Object
+from pycram.worlds.bullet_world import BulletWorld
 
-@with_real_robot
-def move_and_detect(obj_type, navigation_pose, pick_pose):
-    NavigateAction(target_locations=[navigation_pose]).resolve().perform()
+extension = ObjectDescription.get_file_extension()
+world = BulletWorld(WorldMode.DIRECT)
+gripper = HSRBMoveGripperReal()
 
-    LookAtAction(targets=[pick_pose]).resolve().perform()
+# Spawn HSRB
+robot_hsrb = Object("hsrb", ObjectType.ROBOT, f"hsrb{extension}")
+robot_desig_hsrb = ObjectDesignatorDescription(names=["hsrb"]).resolve()
+robot_color = Color(R=0.6, G=0.6, B=0.6, A=1)
+robot_hsrb.set_color(robot_color)
+RobotStateUpdater("/tf", "/hsrb/robot_state/joint_states", multirobot_name='hsrb')
+hsrb_move = PoseNavigator(namespace='hsrb')
 
-    object_desig = DetectAction(BelieveObject(types=[obj_type])).resolve().perform()
+# Spawn Turtle
+robot_turtle = Object("turtlebot", ObjectType.ROBOT, f"turtlebot{extension}")
+robot_desig_turtle = ObjectDesignatorDescription(names=["turtlebot"]).resolve()
+robot_color = Color(R=0.6, G=0.6, B=0.6, A=1)
+robot_turtle.set_color(robot_color)
+turtle_move = PoseNavigator(namespace='turtle')
 
-    return object_desig
+kitchen = Object("kitchen", ObjectType.ENVIRONMENT, "pre_robocup_5.urdf")
+kitchen_desig = ObjectDesignatorDescription(names=["kitchen"])
 
-@with_real_robot
-def hsrb_transport_object_on_turtle():
-    actions(park=True)
-
-    navigation_pose = Pose([0, 0, 0])
-    object_pose = Pose([0.5, 3, 1.02])
-    turtle_surface_pose = Pose([0, 0, 0])
-
-    milk_desig = move_and_detect(ObjectType.MILK, navigation_pose, object_pose)
-    TransportAction(milk_desig, [Arms.LEFT], [turtle_surface_pose]).resolve().perform()
-
-@with_real_robot
-def turtle_navigation():
-    pass
-
-
-def hsrb_transport_object():
-    pass
+# important Publishers
+talk = TextToSpeechPublisher()
+image_switch_publisher = ImageSwitchPublisher()
 
 
-def main():
-    # Environment
-    current_environment = set_environment(ENVIRONMENTS.SUTURO)
-    milk = Object("milk", ObjectType.MILK, "milk.stl", pose=Pose([0.5, 3, 1.02], orientation=[0, 0, 1, 0]),
-                  color=Color(1, 0, 0, 1))
-    milk_BO = BelieveObject(names=["milk"])
-    cereal = Object("cereal", ObjectType.BREAKFAST_CEREAL, "breakfast_cereal.stl",
-                    pose=Pose([0.5, 3.3, 1.05]), color=Color(0, 1, 0, 1))
-    spoon = Object("spoon", ObjectType.SPOON, "spoon.stl", pose=Pose([0.4, 3.2, 0.85]),
-                   color=Color(0, 0, 1, 1))
-    bowl = Object("bowl", ObjectType.BOWL, "bowl.stl", pose=Pose([0.5, 3.2, 1.02]),
-                  color=Color(1, 1, 0, 1))
+def hsrb_transport_object(object_desig, placing_pose):
+    # table_obj = DetectAction(technique='all').resolve().perform()
 
-    # Spawn Robots
-    pose_hsrb = Pose([1.5, 3, 0])
-    pose_turtle = Pose([4, 3, 0])
+    ParkArmsAction(arms=[Arms.LEFT]).resolve().perform()
 
-    hsrb = create_robot(ROBOTS.HSRB, pose=pose_hsrb)
-    turtlebot = create_robot(ROBOTS.TURTLE, pose=pose_turtle)
-    rospy.sleep(3)
+    PickUpAction(object_designator_description=[object_desig], arms=[Arms.LEFT],
+                 grasps=[Grasp.FRONT]).resolve().perform()
 
-    # HSR transporting on turtle
-    print(f"{hsrb.name} actions")
-    with real_robot(hsrb):
-        hsrb_transport_object_on_turtle()
-
-    rospy.sleep(3)
-    # Turtle moving towards goal table
-    print(f"{turtlebot.name} actions")
-    with real_robot(turtlebot):
-        turtle_navigation()
-
-    with real_robot(hsrb):
-        hsrb_transport_object()
+    PlaceAction(object_desig, [placing_pose], Grasp.FRONT, [Arms.LEFT]).resolve().perform()
 
 
-if __name__ == '__main__':
-    main()
+def turtle_turn(angle=0):
+    # Create pose with rotaded Quaternion in turtle frame
+    new_pose = Pose(position=[0, 0, 0], orientation=rotated_quaternion(angle))
+
+    # TODO: Transform Pose into map frame
+
+    NavigateAction(target_locations=[new_pose]).resolve().perform()
+
+
+def turtle_turn_right():
+    turtle_turn(angle=90)
+
+
+def turtle_turn_left():
+    turtle_turn(angle=-90)
+
+
+def turtle_drive_to_table():
+    # TODO: Get correct position
+    goal_position = [2.585, 5.77, 0.0]
+    goal_orientation = rotated_quaternion(angle=90)
+
+    goal_pose = Pose(position=goal_position, orientation=goal_orientation)
+    NavigateAction(target_locations=[goal_pose]).resolve().perform()
+
+
+def demo():
+    milk_position = [2.585, 5.77, 0.8]
+    milk_orientation = rotated_quaternion(angle=90)
+    milk_starting_pose = Pose(position=milk_position, orientation=milk_orientation)
+
+    milk_object = Object("milk", ObjectType.MILK, "milk.stl", pose=milk_starting_pose)
+    milk_desig = ObjectDesignatorDescription.Object(milk_object.name, ObjectType.MILK, milk_object)
+
+    milk_placing_pose = Pose(position=[2.585, 5.77, 0.86], orientation=milk_orientation)
+
+    '''
+    Transport
+    Object:     Milk
+    From:       Table#1
+    To:         Turtlebot 
+    '''
+    with real_robot(robot_hsrb):
+        hsrb_transport_object(object_desig=milk_desig, placing_pose=milk_placing_pose)
+        ParkArmsAction(arms=[Arms.LEFT]).resolve().perform()
+
+    '''
+    Navigate
+    Robot:      Turtlebot
+    From:       Table#1
+    To:         Table#2 
+    '''
+    with real_robot(robot_turtle):
+        turtle_drive_to_table()
+
+    '''
+    Transport
+    Object:     Chips
+    From:       Table#1
+    To:         Table#2 
+    '''
+    with real_robot(robot_hsrb):
+        hsrb_transport_object(object_desig=milk_desig, placing_pose=milk_placing_pose)
+
+    '''
+    Transport
+    Object:     Milk
+    From:       Turtlebot
+    To:         Table#2 
+    '''
+    with real_robot(robot_hsrb):
+        hsrb_transport_object(object_desig=milk_desig, placing_pose=milk_placing_pose)
+        ParkArmsAction(arms=[Arms.LEFT]).resolve().perform()
+
+    print("end")
+
+
+if __name__ == "__main__":
+    demo()
