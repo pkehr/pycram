bdffdd5281464c0e45aaa0b27a0d92b9d408c8ee
pkehr <philk99@web.de>
2025-03-19 16:10:24 +0100

    [fix] adjusted poses, set unique names

--- demos/pycram_multirobot_real_demo/methods/spawn.py
+++ demos/pycram_multirobot_real_demo/methods/spawn.py
@@ -2,9 +2,10 @@ from demos.pycram_multirobot_real_demo.utils import rotated_quaternion
 from pycram.datastructures.dataclasses import Color
 from pycram.datastructures.enums import ObjectType, ROBOTS
 from pycram.datastructures.pose import Pose
-from pycram.description import ObjectDescription
+
 from pycram.designator import ObjectDesignatorDescription
 from pycram.external_interfaces.navigate import PoseNavigator
+from pycram.object_descriptors.urdf import ObjectDescription
 from pycram.world_concepts.world_object import Object
 
 extension = ObjectDescription.get_file_extension()
@@ -27,11 +28,11 @@ def setup_demo_objects():
     starting_orientation_hsrb = rotated_quaternion(angle=90)
     starting_pose_hsrb = Pose(position=starting_position_hsrb, orientation=starting_orientation_hsrb)
 
-    starting_position_turtle = [2.45, 0.969, 0.0]
+    starting_position_turtle = [1.54, 4.27, 0.0]
     starting_orientation_turtle = rotated_quaternion(angle=90)
     starting_pose_turtle = Pose(position=starting_position_turtle, orientation=starting_orientation_turtle)
 
-    table_one_nav_position = [2.45, 0.969, 0.0]
+    table_one_nav_position = [2.4, 4.4, 0.0]
     table_one_nav_orientation = rotated_quaternion(angle=90)
     table_one_nav_pose = Pose(position=table_one_nav_position, orientation=table_one_nav_orientation)
 
@@ -57,7 +58,7 @@ def setup_demo_objects():
     coffee_place_orientation = rotated_quaternion(angle=180)
     coffee_placing_pose = Pose(position=[1.87, 5.24, 0.45], orientation=coffee_place_orientation)
 
-    coffee_object = Object("milk", ObjectType.MILK, "milk.stl", pose=coffee_starting_pose)
+    coffee_object = Object("coffee", ObjectType.MILK, "milk.stl", pose=coffee_starting_pose)
     coffee_desig = ObjectDesignatorDescription.Object(milk_object.name, ObjectType.MILK, coffee_object)
 
     chips_position = [2.885, 5.85, 0.78]
